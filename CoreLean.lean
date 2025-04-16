d96be623e658904faf71115f675b919546fb23de
Lean 4 VS Code Extension <>
2025-04-15 22:27:56 -0400

    Initial commit

---
+++ CoreLean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `CoreLean` library.
+-- Import modules here that should be built as part of the library.
+import CoreLean.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import CoreLean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
